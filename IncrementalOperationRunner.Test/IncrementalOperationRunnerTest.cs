a6f15f21342d3bd9fefd5b672b77ea3c5e7e6f68
Alberto Fustinoni <alberto@aftnet.net>
2015-11-23 16:40:45 +0900

    Initial

---
+++ IncrementalOperationRunner.Test/IncrementalOperationRunnerTest.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IncrementalOperationRunner.Test
+{
+    [TestClass]
+    public class IncrementalOperationRunnerTest
+    {
+        //This needs to be long enough to allow all background tasks to complete
+        const int TestCompletionWaitingTime = 1000;
+        const int BackgroundOperationCancellationCheckStep = 10;
+
+        int BackgroundOperationNumCancellationRequests { get; set; }
+
+        private async Task<int> BackgroundOperationNoCancellation(int input)
+        {
+            await Task.Delay(input);
+            return input;
+        }
+
+        private async Task<int> BackgroundOperationWithancellation(int input, CancellationToken cancellationToken)
+        {
+            var ctr = 0;
+            while (ctr < input)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    BackgroundOperationNumCancellationRequests++;
+                }
+                cancellationToken.ThrowIfCancellationRequested();
+                await Task.Delay(BackgroundOperationCancellationCheckStep);
+                ctr += BackgroundOperationCancellationCheckStep;
+            }
+
+            return input;
+        }
+
+        private IncrementalOperationRunner<int, int> GetRunner(bool cancellationTokenSupport)
+        {
+            if (cancellationTokenSupport)
+            {
+                return new IncrementalOperationRunner<int, int>(BackgroundOperationWithancellation);
+            }
+
+            return new IncrementalOperationRunner<int, int>(BackgroundOperationNoCancellation);
+        }
+
+        private IncrementalOperationRunner<int, int>[] GetRunners()
+        {
+            var vals = new bool[] { true, false };
+            var output = vals.Select(d => GetRunner(d)).ToArray();
+            return output;
+        }
+
+        [TestMethod]
+        public async Task Operation_Is_Executed_As_Expected()
+        {
+            var input = 10;
+            foreach (var runner in GetRunners())
+            {
+                var result = 0;
+                var numTimesEventHandlerCalled = 0;
+                runner.OperationCompletedEvent += d =>
+                {
+                    result = d;
+                    numTimesEventHandlerCalled++;
+                };
+
+                Assert.AreNotEqual(input, result);
+                Assert.AreEqual(0, numTimesEventHandlerCalled);
+                runner.Run(input);
+                Assert.AreNotEqual(input, result);
+                Assert.AreEqual(0, numTimesEventHandlerCalled);
+
+                //Simulate idle UI thread
+                await Task.Delay(TestCompletionWaitingTime);
+
+                Assert.AreEqual(input, result);
+                Assert.AreEqual(1, numTimesEventHandlerCalled);
+            }
+        }
+
+        [TestMethod]
+        public async Task Operations_Called_Before_Previous_Ends_Do_Not_Raise_Completion_Event()
+        {
+            foreach (var runner in GetRunners())
+            {
+                var waitTimeBetweenInputs = 30;
+                var inputs = new int[] { 2 * waitTimeBetweenInputs, 5 * waitTimeBetweenInputs, 3 * waitTimeBetweenInputs };
+
+                var result = 0;
+                var numTimesEventHandlerCalled = 0;
+                runner.OperationCompletedEvent += d =>
+                {
+                    result = d;
+                    numTimesEventHandlerCalled++;
+                };
+
+                foreach (var i in inputs)
+                {
+                    Assert.AreNotEqual(i, result);
+                    Assert.AreEqual(0, numTimesEventHandlerCalled);
+                    runner.Run(i);
+                    Assert.AreNotEqual(i, result);
+                    Assert.AreEqual(0, numTimesEventHandlerCalled);
+                    await Task.Delay(waitTimeBetweenInputs);
+                }
+
+                //Simulate idle UI thread
+                await Task.Delay(TestCompletionWaitingTime);
+
+                Assert.AreEqual(inputs.Last(), result);
+                Assert.AreEqual(1, numTimesEventHandlerCalled);
+            }
+        }
+
+        [TestMethod]
+        public async Task Operation_Is_Canceled_If_A_New_One_Is_Requested()
+        {
+            var waitTimeBetweenInputs = 30;
+            var inputs = new int[] { 5 * waitTimeBetweenInputs, 5 * waitTimeBetweenInputs, 2 * waitTimeBetweenInputs };
+
+            var runner = GetRunner(true);
+            var result = 0;
+            var numTimesEventHandlerCalled = 0;
+            BackgroundOperationNumCancellationRequests = 0;
+            runner.OperationCompletedEvent += d =>
+            {
+                result = d;
+                numTimesEventHandlerCalled++;
+            };
+
+            foreach (var i in inputs)
+            {
+                runner.Run(i);
+                await Task.Delay(waitTimeBetweenInputs);
+            }
+
+            //Simulate idle UI thread
+            await Task.Delay(TestCompletionWaitingTime);
+
+            Assert.AreEqual(inputs.Last(), result);
+            Assert.AreEqual(1, numTimesEventHandlerCalled);
+            Assert.AreEqual(inputs.Length - 1, BackgroundOperationNumCancellationRequests);
+        }
+
+        [TestMethod]
+        public async Task Calling_Stop_Cancels_Operation_Witout_Raising_Completion_event()
+        {
+            var waitTimeBetweenInputs = 30;
+            var initialResultValue = -1;
+            var input = 10 * waitTimeBetweenInputs;
+
+            var runner = GetRunner(true);
+            var result = initialResultValue;
+            var numTimesEventHandlerCalled = 0;
+            BackgroundOperationNumCancellationRequests = 0;
+            runner.OperationCompletedEvent += d =>
+            {
+                result = d;
+                numTimesEventHandlerCalled++;
+            };
+
+            runner.Run(input);
+            await Task.Delay(waitTimeBetweenInputs);
+            runner.Stop();
+
+            //Simulate idle UI thread
+            await Task.Delay(TestCompletionWaitingTime);
+
+            Assert.AreEqual(initialResultValue, result);
+            Assert.AreEqual(0, numTimesEventHandlerCalled);
+            Assert.AreEqual(1, BackgroundOperationNumCancellationRequests);
+        }
+
+        [TestMethod]
+        public async Task Operations_Can_Be_Started_After_Stopping()
+        {
+            var input = 100;
+            foreach (var waitTimeBetweenInputs in new int[] { input / 2, input * 2 })
+            {
+                var initialResultValue = -1;
+
+                var runner = GetRunner(true);
+                var result = initialResultValue;
+                var numTimesEventHandlerCalled = 0;
+                BackgroundOperationNumCancellationRequests = 0;
+                runner.OperationCompletedEvent += d =>
+                {
+                    result = d;
+                    numTimesEventHandlerCalled++;
+                };
+
+                runner.Run(input);
+                await Task.Delay(waitTimeBetweenInputs);
+                runner.Stop();
+                await Task.Delay(waitTimeBetweenInputs);
+                runner.Run(input);
+
+                //Simulate idle UI thread
+                await Task.Delay(TestCompletionWaitingTime);
+
+                Assert.AreEqual(input, result);
+                var operationShouldHaveBeenCanceled = waitTimeBetweenInputs < input;
+                var expectedNumCancellations = operationShouldHaveBeenCanceled ? 1 : 0;              
+                Assert.AreEqual(expectedNumCancellations, BackgroundOperationNumCancellationRequests);
+                var expectedNumCallbacks = operationShouldHaveBeenCanceled ? 1 : 2;
+                Assert.AreEqual(expectedNumCallbacks, numTimesEventHandlerCalled);
+            }
+        }
+    }
+}
---
+++ IncrementalOperationRunner/IncrementalOperationRunner.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IncrementalOperationRunner
+{
+    /// <summary>
+    /// Encapsulates the logic needed to perform incremental operations, that is operations for which we are only interested in the output of the most recent input.
+    /// Most common use case is incremental search, where the search operation takes time and must be run in the background.
+    /// After calling run, a new background task is started to carry out the operation as defined in the background.
+    /// Once finished, the operation completed event specified will be called in the same synchronization context as the call to run.
+    /// If run is called again while a background operation is running, a cancellation request is sent to the background task:
+    /// in that case, once the task returns another background task running the operation on the current input value is started instead of calling the completion event.
+    /// </summary>
+    /// <typeparam name="TInput">Input of the background opertaion</typeparam>
+    /// <typeparam name="TOutput">Output of the background operation</typeparam>
+    public class IncrementalOperationRunner<TInput, TOutput>
+    {
+        private readonly Func<TInput, CancellationToken, Task<TOutput>> BackgroundOperation;
+        private TInput CurrentInput { get; set; }
+        private CancellationTokenSource CTSource { get; set; }
+        private bool StopRequested { get; set; }
+
+        public event Action<TOutput> OperationCompletedEvent;
+
+        public IncrementalOperationRunner(Func<TInput, CancellationToken, Task<TOutput>> backgroundOperation)
+        {
+            BackgroundOperation = backgroundOperation;
+        }
+
+        public IncrementalOperationRunner(Func<TInput, Task<TOutput>> backgroundOperation)
+        {
+            BackgroundOperation = (d, e) => backgroundOperation(d);
+        }
+
+        public void Run(TInput input)
+        {
+            StopRequested = false;
+            CurrentInput = input;
+            InputChanged();
+        }
+
+        public void Stop()
+        {
+            StopRequested = true;
+            if (CTSource != null)
+            {
+                CTSource.Cancel();
+            }
+        }
+
+        private async void InputChanged()
+        {
+            if (CTSource != null)
+            {
+                CTSource.Cancel();
+                return;
+            }
+
+            var searchResult = default(TOutput);
+
+            bool shouldContinue = true;
+            while (shouldContinue)
+            {
+                using (CTSource = new CancellationTokenSource())
+                {
+                    try
+                    {
+                        searchResult = await BackgroundOperation(CurrentInput, CTSource.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                    }
+                    shouldContinue = CTSource.IsCancellationRequested && !StopRequested;
+                }
+                CTSource = null;
+            }
+
+            if(!StopRequested)
+            {
+                OperationCompletedEvent(searchResult);
+            }
+        }
+    }
+}
